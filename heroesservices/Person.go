076a45e997bfb698d1cfa70ed3a50343d0724f96
Ricardo Serradas <Ricardo.Serradas@microsoft.com>
2018-05-07 14:35:40 -0300

    Person already returning hero name through API

--- heroesservices/Person.go
+++ heroesservices/Person.go
@@ -1,5 +1,9 @@
 package heroesservices
 
+import (
+	"strings"
+)
+
 type Person struct {
 	FirstName string
 	LastName  string
@@ -10,10 +14,72 @@ func (person *Person) SetHeroName() {
 	person.HeroName = GetFirstName(person.FirstName) + " " + GetLastName(person.LastName)
 }
 
-func GetFirstName(name string) string {
-	return "Hata"
+func GetLastName(name string) string {
+	m := make(map[string]string)
+
+	m["A"] = "Lightning"
+	m["B"] = "Knight"
+	m["C"] = "Hulk"
+	m["D"] = "Centurion"
+	m["E"] = "Surfer"
+	m["F"] = "Girl"
+	m["G"] = "Warrior"
+	m["H"] = "Man"
+	m["I"] = "Ghost"
+	m["J"] = "Master"
+	m["K"] = "Hornet"
+	m["L"] = "Phantom"
+	m["M"] = "Crusader"
+	m["N"] = "Daredevil"
+	m["O"] = "Machine"
+	m["P"] = "America"
+	m["Q"] = "X"
+	m["R"] = "Doom"
+	m["S"] = "First"
+	m["T"] = "Shadow"
+	m["U"] = "Patriot"
+	m["V"] = "Claw"
+	m["W"] = "Panther"
+	m["X"] = "Hawk"
+	m["Y"] = "Storm"
+	m["Z"] = "Thunder"
+
+	lastHeroName := m[strings.ToUpper(string(name[0]))]
+
+	return lastHeroName
 }
 
-func GetLastName(name string) string {
-	return "Mahata"
+func GetFirstName(name string) string {
+	m := make(map[string]string)
+
+	m["A"] = "Captain"
+	m["B"] = "Night"
+	m["C"] = "The"
+	m["D"] = "Ancient"
+	m["E"] = "Spider"
+	m["F"] = "Invisible"
+	m["G"] = "Master"
+	m["H"] = "Mr"
+	m["I"] = "Silver"
+	m["J"] = "Dark"
+	m["K"] = "Professor"
+	m["L"] = "Radioactive"
+	m["M"] = "Incredible"
+	m["N"] = "Impossible"
+	m["O"] = "Iron"
+	m["P"] = "Rocket"
+	m["Q"] = "Human"
+	m["R"] = "Power"
+	m["S"] = "Green"
+	m["T"] = "Super"
+	m["U"] = "Wonder"
+	m["V"] = "Metal"
+	m["W"] = "Doctor"
+	m["X"] = "Masked"
+	m["Y"] = "Crimson"
+	m["Z"] = "Omega"
+
+	firstHeroName := m[strings.ToUpper(string(name[0]))]
+
+	return firstHeroName
 }
